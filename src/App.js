17ab4042095faa4d3b9c63bd8be4608e0dd6c266
aripan <aripan017@gmail.com>
2021-04-30 19:14:39 +0200

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header />
       <Home />
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,31 +2,29 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <main id="home">
-        <h1 className="lg-heading">
-          John
-          <span className="text-secondary">Doe</span>
-        </h1>
-        <h2 className="sm-heading">
-          Web Developer, Programmer, Designer & Entrepreneur
-        </h2>
-        <div className="icons">
-            <a href="#!">
-                <i className="fab fa-twitter fa-2x"></i>
-            </a>
-            <a href="#!">
-                <i className="fab fa-facebook fa-2x"></i>
-            </a>
-            <a href="#!">
-                <i className="fab fa-linkedin fa-2x"></i>
-            </a>
-            <a href="#!">
-                <i className="fab fa-github fa-2x"></i>
-            </a>
-        </div>
-      </main>
-    </div>
+    <main id="home">
+      <h1 className="lg-heading">
+        John
+        <span className="text-secondary"> Doe</span>
+      </h1>
+      <h2 className="sm-heading">
+        Web Developer, Programmer, Designer & Entrepreneur
+      </h2>
+      <div className="icons">
+        <a href="#!">
+          <i className="fab fa-twitter fa-2x"></i>
+        </a>
+        <a href="#!">
+          <i className="fab fa-facebook fa-2x"></i>
+        </a>
+        <a href="#!">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="#!">
+          <i className="fab fa-github fa-2x"></i>
+        </a>
+      </div>
+    </main>
   );
 };
 
