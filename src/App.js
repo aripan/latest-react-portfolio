b465a046f59767ae75b5b630a7b376a81f0acc00
aripan <aripan017@gmail.com>
2021-05-01 17:58:55 +0200

    About Page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from "./components/About/About";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 
@@ -6,6 +7,7 @@ function App() {
     <div>
       <Header />
       <Home />
+      <About />
     </div>
   );
 }
