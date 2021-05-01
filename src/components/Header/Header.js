1e23aebf4f0739093ec50b3b68bb5a78331c7f48
aripan <aripan017@gmail.com>
2021-05-01 15:59:09 +0200

    menu btn & rotate

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <div>
       <header>
-        <div className="menu-btn">
+        <div
+          onClick={toggleMenu}
+          className={`menu-btn ${showMenu ? "close" : ""}`}
+          // className="menu-btn"
+        >
           <div className="btn-line"></div>
           <div className="btn-line"></div>
           <div className="btn-line"></div>
         </div>
 
-        <nav className="menu">
-          <div className="menu-branding">
+        <nav className={`menu ${showMenu ? "show" : ""}`}>
+          <div className={`menu-branding ${showMenu ? "show" : ""}`}>
             <div className="portrait"></div>
           </div>
-          <ul className="menu-nav">
-            <li className="nav-item current">
+          <ul className={`menu-nav ${showMenu ? "show" : ""}`}>
+            <li className={`nav-item current ${showMenu ? "show" : ""}`}>
               <a href="#!" className="nav-link">
                 Home
               </a>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${showMenu ? "show" : ""}`}>
               <a href="#!" className="nav-link">
                 About Me
               </a>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${showMenu ? "show" : ""}`}>
               <a href="#!" className="nav-link">
                 My Work
               </a>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${showMenu ? "show" : ""}`}>
               <a href="#!" className="nav-link">
                 How To Reach Me
               </a>
